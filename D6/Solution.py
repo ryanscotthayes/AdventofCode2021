0f082a15d2ac6b9fadb53ee2dd56d57d5efdd8cd
Ryan Hayes <ryanscotthayes@gmail.com>
2021-12-06 02:46:29 -0600

    D6Q2 should be finished. Stable commit b4 tweak

---
+++ D6/Solution.py
@@ -0,0 +1,79 @@
+#file_path = 'D6/input.txt'
+file_path = 'D6/test.txt'
+with open(file_path) as f:
+    text = f.read()
+
+data = text.split(',')
+for i in range(0,len(data)):
+    data[i] = int(data[i])
+
+num_days = 80
+
+for day in range(0,num_days):
+    num_fish_created = 0
+    for i in range(0,len(data)):
+        if data[i] == 0:
+            data[i] = 6
+            num_fish_created +=1
+        else:
+            data[i] = data[i]-1
+    
+    for i in range(0,num_fish_created):
+        data.append(8)
+    #print('Day : ', day)
+
+print('Part 1: ' + str(len(data)))
+
+
+#############################################
+
+data = text.split(',')
+
+num_days = 80
+agg_list = list(range(0,9))
+for i in range(0,len(agg_list)):
+    agg_list[i] = [i,0]
+
+
+for j in range(0,len(data)):
+    for k in range(0,len(agg_list)):
+        
+        if int(data[j]) == agg_list[k][0]:
+            agg_list[k][1] +=1
+
+
+
+
+print(agg_list)
+for day in range(0,num_days):
+    for i in range(0,len(agg_list)):
+        if agg_list[i][0] == 0: # Code for recently repopulated fish
+            agg_list[i] = [99,agg_list[i][1]]
+            num_fish_created = agg_list[i][1]
+        else:
+            agg_list[i] = [agg_list[i][0]-1,agg_list[i][1]]
+    agg_list.append([8,0])
+    for i in range(0,num_fish_created):
+        agg_list[-1][1] += 1
+    
+    new_agg_list = []
+    for i in range(0,len(agg_list)):
+        
+        if agg_list[i][0] == 99:
+            reproduce_count = agg_list[i][1]
+        else:
+            new_agg_list.append(agg_list[i])
+
+    for i in range(0,len(new_agg_list)):
+        if new_agg_list[i][0] == 6:
+            new_agg_list[i][1] += reproduce_count
+    #print('Day {} Finished',day)
+    agg_list = new_agg_list
+
+solution = 0
+for i in range(0,len(agg_list)):
+    solution += agg_list[i][1]
+
+print(agg_list)
+print('Part 2: ' + str(solution))
+
