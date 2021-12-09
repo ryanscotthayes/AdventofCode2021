ab834e93bc6e8680de3115bd1d159d181607db37
Ryan Hayes <ryanscotthayes@gmail.com>
2021-12-09 15:25:42 -0600

    The problem specified "basins" not "bastions"

--- D9/Solution.py
+++ D9/Solution.py
@@ -51,11 +51,11 @@ for y_pos in range(0,len(data)):
         if int(data[y_pos][x_pos]) < min(return_array):
             low_points.append([x_pos,y_pos])
 
-bastionPoints = []
+basinPoints = []
 for i in range(0,len(low_points)):
     new_point_array = []
-    bastion_existing_points = []
-    bastion_existing_points.append(low_points[i])
+    basin_existing_points = []
+    basin_existing_points.append(low_points[i])
     point_array = getAdjacentPointCoords(low_points[i][0],low_points[i][1],data)
     for i in range(0,len(point_array)):
         if data[point_array[i][1]][point_array[i][0]] == '9':
@@ -63,38 +63,38 @@ for i in range(0,len(low_points)):
         else:
             new_point_array.append(point_array[i])
     for i in new_point_array:
-        bastion_existing_points.append(i)
-    bastionPoints.append(bastion_existing_points)
+        basin_existing_points.append(i)
+    basinPoints.append(basin_existing_points)
 
-prev_max_bastion_size = 0 
-for i in bastionPoints:
-    if len(i)> prev_max_bastion_size:
-        prev_max_bastion_size = len(i)
+prev_max_basin_size = 0 
+for i in basinPoints:
+    if len(i)> prev_max_basin_size:
+        prev_max_basin_size = len(i)
 
-max_bastion_size = prev_max_bastion_size + 1 # This is just to start the loop
-while max_bastion_size > prev_max_bastion_size:
-    prev_max_bastion_size = max_bastion_size
-    for i in range(0,len(bastionPoints)):
-        for j in range(0,len(bastionPoints[i])):
-            next_point_array = getAdjacentPointCoords(bastionPoints[i][j][0],bastionPoints[i][j][1],data)
-            # print(bastionPoints[i])
+max_basin_size = prev_max_basin_size + 1 # This is just to start the loop
+while max_basin_size > prev_max_basin_size:
+    prev_max_basin_size = max_basin_size
+    for i in range(0,len(basinPoints)):
+        for j in range(0,len(basinPoints[i])):
+            next_point_array = getAdjacentPointCoords(basinPoints[i][j][0],basinPoints[i][j][1],data)
+            # print(basinPoints[i])
             for k in next_point_array:
-                if k in bastionPoints[i]:
+                if k in basinPoints[i]:
                     pass
                 else:
                     if data[k[1]][k[0]] != '9':
-                        bastionPoints[i].append(k)
+                        basinPoints[i].append(k)
                     else: pass
-    max_bastion_size = prev_max_bastion_size
-    for i in bastionPoints:
-        if len(i)> max_bastion_size:
-            max_bastion_size = len(i)
+    max_basin_size = prev_max_basin_size
+    for i in basinPoints:
+        if len(i)> max_basin_size:
+            max_basin_size = len(i)
 
-bastion_sizes = []
-for i in range(0,len(bastionPoints)):
-    bastion_sizes.append(len(bastionPoints[i]))
+basin_sizes = []
+for i in range(0,len(basinPoints)):
+    basin_sizes.append(len(basinPoints[i]))
 
 solution = 1
-for i in sorted(bastion_sizes)[-3:]:
+for i in sorted(basin_sizes)[-3:]:
     solution *= i
 print('Pt2: ', solution)
